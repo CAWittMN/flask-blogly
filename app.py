23ecc8514655d28df18dc56f398df8fedef2ecce
Casey Wittrock <caseywittrockmn@gmail.com>
2023-04-28 01:18:36 -0500

    boilerplate and requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
