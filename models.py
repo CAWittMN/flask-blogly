6af1660fefeb9c55a66281ce8eb1f325a0fe7a0f
Casey Wittrock <caseywittrockmn@gmail.com>
2023-05-02 22:14:44 -0500

    start html templates

--- models.py
+++ models.py
@@ -4,11 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-
 def connect_db(app):
     """Connect to database."""
-
-    db.app = app
     db.init_app(app)
 
 
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/user-login-icon-14.png"
+
+
+class User(db.Model):
+    """User model"""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Column(db.Text, nullable=False, unique=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
