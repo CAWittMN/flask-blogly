5421949cea1abd16a392a68683ea90422fbfe48a
Casey Wittrock <caseywittrockmn@gmail.com>
2023-05-03 10:10:58 -0500

    fix psycopg

--- app.py
+++ app.py
@@ -2,11 +2,29 @@
 
 from flask import Flask, request, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, User, connect_db
+from models import User, db, connect_db
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
+app.config['SECRET_KEY'] = 'bloglysecretkey'
+
+toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def root():
+    return render_template('home.html')
+
+@app.route('/users')
+def show_users():
+    return render_template('users.htm')
+
+@app.route('/new-user')
+def new_user():
+    return render_template('new-user')
