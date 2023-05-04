8b8a5a7eb8340d32b2cebd239910005d4d15611d
Casey Wittrock <caseywittrockmn@gmail.com>
2023-05-04 11:24:04 -0500

    finish basic add user logic

--- app.py
+++ app.py
@@ -8,41 +8,52 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
-app.config['SECRET_KEY'] = 'bloglysecretkey'
+app.config["SECRET_KEY"] = "bloglysecretkey"
 
-toolbar = DebugToolbarExtension(app)
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 
 with app.app_context():
     db.create_all()
 
-@app.route('/')
-def root():
-    return render_template('home.html')
 
-@app.route('/users')
+@app.route("/")
+def root():
+    return render_template("home.html")
+
+
+@app.route("/users")
 def show_users():
     users = User.query.all()
     if len(users) == 0:
-        flash('No users')
-        return redirect('/')
-    return render_template('users.html', users=users)
+        flash("No users")
+        return redirect("/")
+    return render_template("users.html", users=users)
 
-@app.route('/new-user', methods=['GET'])
+
+@app.route("/new-user", methods=["GET"])
 def new_user():
-    return render_template('new-user.html')
+    return render_template("new-user.html")
 
-@app.route('/new-user', methods=['POST'])
+
+@app.route("/new-user", methods=["POST"])
 def add_user():
     new_user = User(
-        user_name=request.form['user_name'], 
-        first_name=request.form['first_name'], 
-        last_name=request.form['last_name'], 
-        image_url=request.form['image_url'] or None)
-    
+        user_name=request.form["user_name"],
+        first_name=request.form["first_name"],
+        last_name=request.form["last_name"],
+        image_url=request.form["image_url"] or None,
+    )
+
     db.session.add(new_user)
     db.session.commit()
 
-    flash('User added!')
-    return redirect('/new-user')
+    flash("User added!")
+    return redirect("/new-user")
+
+
+@app.route("/users/<int:user_id>")
+def show_user():
+    user = User.query.get_or_404(user_id)
+    return render_template("show-user.html", user=user)
