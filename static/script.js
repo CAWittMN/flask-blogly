3db6cdb2b6202805e3ebd16ad9cbee2d3bcb9de6
Casey Wittrock <caseywittrockmn@gmail.com>
2023-05-14 12:00:37 -0500

    finish js script for tag input

--- static/script.js
+++ static/script.js
@@ -1,34 +1,89 @@
-class Tag {
+class TagInput {
   constructor() {
-    this.$tagRow = $("#tag-row");
+    this.$tagInputCol = $("#form-col");
+    this.makeInput();
     this.$tagForm = $("#tag-form");
     this.$tagInput = $("#tag-input");
 
-    this.$tagForm.on("submit", this.handleTagSubmit.bind(this));
+    this.$tagForm.on("submit", this.handleSubmit.bind(this));
+  }
+  handleSubmit(evt) {
+    evt.preventDefault();
+    const tagName = this.$tagInput.val().toLowerCase().split(" ").join("");
+    console.log(tagName);
+    if (!this.checkTagExists(tagName)) {
+      new Tag(tagName);
+    } else {
+      $(`#tag_${tagName}`).prop("checked", true);
+    }
+    this.$tagInput.val("");
   }
 
-  handleTagSubmit(evt) {
-    evt.preventDefault();
-    const tagName = this.$tagInput.val().toLowerCase();
-    const $newCol = $("<div>").attr({ class: "col" });
+  checkTagExists(tagName) {
+    if ($(`#tag_${tagName}`).length) {
+      return true;
+    }
+    return false;
+  }
+
+  makeInput() {
+    const $tagForm = $("<form>").attr("id", "tag-form");
+    const $tagInputDiv = $("<div>").attr("class", "form-floating");
+    const $newTagInput = $("<input>").attr({
+      id: "tag-input",
+      type: "text",
+      name: "tag",
+      class: "form-control",
+      placeholder: "Tag name",
+    });
+    const $tagLabel = $("<label>").attr("for", "tag").text("Enter a tag");
+
+    this.$tagInputCol.append(
+      $tagForm.append($tagInputDiv.append($newTagInput, $tagLabel))
+    );
+  }
+}
+class Tag {
+  constructor(tagName) {
+    this.tagName = tagName;
+    this.$tagsRow = $("#tags-row");
+    this.tagId = "";
+    this.initTag();
+  }
+  async initTag() {
+    await this.postTag();
+
+    this.createTagPill();
+  }
+
+  createTagPill() {
+    const $newCol = $("<div>").attr({ class: "col mb-1 py-o pe-1 ps-0" });
     const $newTagBtn = $("<input>").attr({
-      value: `${tagName}`,
+      form: "post-form",
+      value: `${this.tagName}`,
       type: "checkbox",
       class: "btn-check",
-      id: `tag-${tagName}`,
+      id: `tag_${this.tagName}`,
       autocomplete: "off",
       name: "tags",
       checked: true,
     });
     const $newTagLbl = $("<label>")
       .attr({
-        for: `tag-${tagName}`,
-        class: "btn btn-sm btn-outline-primary",
+        for: `tag_${this.tagName}`,
+        class:
+          "btn pt-0 pb-0 pr-1 pl-1 btn-sm rounded-pill btn-outline-primary",
       })
-      .text(`${tagName}`);
+      .text(`${this.tagName}`);
     $newCol.append($newTagBtn, $newTagLbl);
-    this.$tagRow.append($newCol);
+    this.$tagsRow.append($newCol);
+  }
+
+  async postTag() {
+    const postTag = await axios.post("/add-tag", { tag_name: this.tagName });
   }
 }
 
-tag = new Tag();
+if ($("#form-col").length) {
+  new TagInput();
+}
